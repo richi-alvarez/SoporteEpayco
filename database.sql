b063722dc778267525f54ea5555bc59e90cf21d9
pruebasepayco <39378263+richi-alvarez@users.noreply.github.com>
2021-03-27 20:12:10 -0500

    add InvoiceController and create sql sintax
    add InvoiceController and create sql sintax

---
+++ database.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE IF NOT EXISTS api_rest_recaudo_en_linea;
+USE api_rest_recaudo_en_linea;
+
+CREATE TABLE users (
+    id int(255) auto_increment not null,
+    name varchar(50) not null,
+    email varchar(255) not null,
+    password varchar(255) not null,
+    created_at datetime DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT pk_users PRIMARY KEY(id)
+) ENGINE=InnoDb;
+
+CREATE TABLE invoices(
+    id int(255) auto_increment not null,
+    user_id int(255) not null, 
+    created_at datetime DEFAULT CURRENT_TIMESTAMP,
+    updated_at datetime DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT pk_invoices PRIMARY KEY(id),
+    CONSTRAINT fk_invoice_user FOREIGN KEY(user_id) REFERENCES users(id)
+)ENGINE=InnoDb;
